194851e03fff422a2619a64e807de8755932768e
Grzegorz Grybisz <ggrybisz@gmail.com>
2012-10-25 21:33:30 +0200

    Initial commit, very simple project stub

---
+++ SimpleApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace proj
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon();
+        public MainWindow()
+        {
+            InitializeComponent();
+            notifyIcon.Text = Name;
+            
+            //notifyIcon.BalloonTipTitle = "title";          
+            notifyIcon.Icon = new System.Drawing.Icon("Game.ico");
+            notifyIcon.Visible = true;
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+             wpiszBox a = new wpiszBox();
+             a.ShowDialog();
+             mainTextBlock.Text += a.addTextBox.Text + "\n";
+             notifyIcon.BalloonTipText = a.addTextBox.Text;
+             
+             notifyIcon.ShowBalloonTip(3); 
+        }
+    }
+}
---
+++ SimpleApp/wpiszBox.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace proj
+{
+    /// <summary>
+    /// Interaction logic for wpiszBox.xaml
+    /// </summary>
+    public partial class wpiszBox : Window
+    {
+        public wpiszBox()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {            
+            Close();
+        }
+    }
+}

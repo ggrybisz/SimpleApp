91a9d1e35579281b0be710b4f5b7812c064a2c36
Grzegorz Grybisz <ggrybisz@gmail.com>
2012-12-04 08:36:26 +0100

    added installer and some tests

---
+++ TestProject/MainWindowTest.cs
@@ -0,0 +1,29 @@
+﻿using proj;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    
+    
+    /// <summary>
+    ///This is a test class for MainWindowTest and is intended
+    ///to contain all MainWindowTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class MainWindowTest
+    {
+
+
+        /// <summary>
+        ///A test for TimeLess
+        ///</summary>
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TimeLessTest()
+        {
+            int t = 30; 
+            MainWindow.TimeLess(t);
+        }
+    }
+}
--- TestProject/Properties/AssemblyInfo.cs
+++ TestProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestProject1")]
+[assembly: AssemblyTitle("TestProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestProject1")]
+[assembly: AssemblyProduct("TestProject")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("85d63d67-1daa-497a-95f7-8f3b4f0b0b77")]
+[assembly: Guid("ea324b1e-9048-4609-af7e-04d5ad9ea097")]
 
 // Version information for an assembly consists of the following four values:
 //
